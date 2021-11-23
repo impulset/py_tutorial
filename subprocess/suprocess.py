f8f3401a5d5b1773df53e4c7398c2966a15af623
Pavel Sizykh <sizykh86@gmail.com>
2021-11-23 00:15:32 -0500

    edited subprocess

--- subprocess/suprocess.py
+++ subprocess/suprocess.py
@@ -6,4 +6,16 @@ ping_command = subprocess.run(['ping', '-c 2', '-a', f'{ip_address}'], capture_o
 print(ping_command.stdout) #stdout allows access command output
 print(ping_command.returncode) #if command completed succesfully returns 0
 
-print(subprocess.run(['rm', '-r', 'test']).returncode)
+def create_folder(name):
+    print("Creating folder.....")
+    result = subprocess.run(['mkdir', f'{name}']).returncode
+    if result == 0:
+        print(f'{name} was successfully created')
+
+def remove_folder(name):
+    result = subprocess.run(['rm', '-r', f'{name}']).returncode
+    if result == 0:
+        print(f'{name} was successfully removed')
+
+remove_folder("new_test1")
+
